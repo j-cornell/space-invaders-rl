3187c33c4f87e9cd25cb4b3c84effcabb010386f
Jakob Cornell <jcornell@oberlin.edu>
2017-04-29 01:40:45 -0400

    Implement reduced state and reduction function

---
+++ reduction.py
@@ -0,0 +1,34 @@
+#import vision
+import math
+
+PLAYER_BITS = 3
+BULLET_BITS = 16
+
+class ReducedState(object):
+	def __init__(self, player_position, bullet_positions):
+		self.player_position = reduce_player(player_position)
+		self.bullet_positions = set(reduce_bullet(pos, player_position) for pos in bullet_positions)
+
+	def __eq__(self, other):
+		return self.player_position == other.player_position and self.bullet_positions == other.bullet_positions
+
+	def __hash__(self):
+		return hash((self.player_position, self.bullet_positions))
+
+def reduce_player(position):
+	return int((position - vision.PLAYER_X_MIN) / (vision.PLAYER_X_MAX - vision.PLAYER_X_MIN) * 2 ** PLAYER_BITS)
+
+def reduce_bullet(position, player_position):
+	(px, py) = player_position
+	resolution = 2 ** (BULLET_BITS / 2)
+
+	def angle(position):
+		(x, y) = position
+		return int(math.atan2(y - py, x - px) / math.pi * resolution)
+
+	def radius(position):
+		max_radius = math.sqrt((vision.BULLET_RIGHT - vision.BULLET_LEFT) ** 2 + (vision.BULLET_TOP - vision.BULLET_BOTTOM) ** 2)
+		radius = math.sqrt((x - px) ** 2 + (y - py) ** 2)
+		return int(math.log(radius) / math.log(max_radius) * resolution)
+	
+	return (radius(position), angle(position))
